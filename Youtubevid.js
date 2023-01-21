1d22b9e729553a34840dbe49d847b586b87fafc9
etouch33 <92264212+etouch33@users.noreply.github.com>
2023-01-21 18:34:56 +0200

    Update Youtubevid.js

--- Youtubevid.js
+++ Youtubevid.js
@@ -1,3 +1,5 @@
 var ifrm = document.createElement("iframe");
 ifrm.src = document.getElementById("ytburl").href.replace("watch?v=", "embed/");
+ifrm.style.height = "300px";
+ifrm.style.width = "500px";
 document.getElementById("ifrmholder").appendChild(ifrm);
